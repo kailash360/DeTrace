3bf70c3b19443c648a94e10e99265c7367b9a60d
kailash360 <kailashkejriwal21@gmail.com>
2022-04-10 03:08:14 +0530

    added function to release

--- frontend/src/utils/GetContract.js
+++ frontend/src/utils/GetContract.js
@@ -1,8 +1,9 @@
 import Constants from '../Constants'
 import Web3 from 'web3'
 const DETRACE_CONTRACT_BUILD = require('../build/DeTrace.json')
-const web3 = new Web3(Constants.RPC_PROVIDER)
 const NETWORK_ID = "5777"
+
+export const web3 = new Web3(Constants.RPC_PROVIDER)
 
 const GetContract = () => {
     const CONTRACT_ADDRESS = DETRACE_CONTRACT_BUILD.networks[NETWORK_ID].address
