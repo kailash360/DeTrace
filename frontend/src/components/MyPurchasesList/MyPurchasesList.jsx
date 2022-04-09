4a79e72cad100155d055f59c2039e591f80371d9
kailash360 <kailashkejriwal21@gmail.com>
2022-04-10 00:04:38 +0530

    added function for purchases

--- frontend/src/components/MyPurchasesList/MyPurchasesList.jsx
+++ frontend/src/components/MyPurchasesList/MyPurchasesList.jsx
@@ -1,53 +1,31 @@
 import React from 'react'
 import ProductListTemplate from '../ProductListTemplate/ProductListTemplate';
-
-
-const productList = [
-    {
-        name: 'CHakku',
-        id: '123',
-        price: 200,
-        ipfsHash: 'test',
-        stage: 'Sold',
-        manufacturer: '0xxxx',
-        retailerList: ['Oxxx1','Oxxx2' ],
-        customer: '0xxx4'
-    },
-    {
-        name: 'CHakku',
-        id: '123',
-        price: 200,
-        ipfsHash: 'test',
-        stage: 'Sold',
-        manufacturer: '0xxxx',
-        retailerList: ['Oxxx1','Oxxx2' ],
-        customer: '0xxx4'
-    },
-    {
-        name: 'CHakku',
-        id: '123',
-        price: 200,
-        ipfsHash: 'test',
-        stage: 'Sold',
-        manufacturer: '0xxxx',
-        retailerList: ['Oxxx1','Oxxx2' ],
-        customer: '0xxx4'
-    },
-    {
-        name: 'CHakku',
-        id: '123',
-        price: 200,
-        ipfsHash: 'test',
-        stage: 'Sold',
-        manufacturer: '0xxxx',
-        retailerList: ['Oxxx1','Oxxx2' ],
-        customer: '0xxx4'
-    },
-]
+import { AuthContext } from '../../context/AuthContext'
+import { ContractContext } from '../../context/ContractContext'
 
 const MyPurchasesList = () => {
+
+  const { account } = React.useContext(AuthContext)
+  const { Services } = React.useContext(ContractContext)
+
+  const [products, setProducts] = React.useState([])
+
+  const getPurchases = async () => {
+    
+    const purchasesResponse = await Services.getPurchases(account)
+    if(!purchasesResponse.success) console.log(purchasesResponse.message)
+
+    setProducts(purchasesResponse.data.products)
+  }
+
+  React.useEffect(() => {
+    getPurchases()
+  },[account, Services])
+
   return (
-    <ProductListTemplate title={'My Purchases'} productList={productList} />
+    products.length ?
+      <ProductListTemplate title={'My Purchases'} productList={products} />
+    : <p>No purchases yet</p>
   )
 }
 
